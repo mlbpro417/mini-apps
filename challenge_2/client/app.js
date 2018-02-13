3267a9a8a5b334d4b4f1c8e7a163e93a899378a6
mlbpro417 <mlbpro417@gmail.com>
2018-02-13 10:49:42 -0800

    initial commit including basic server setup and html files

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,11 @@
+/*For basic requirements, data-entry of the JSON on the client should be done using an HTML form with a single 
+textarea input field (for the entire JSON text) and a submit button. 
+When the user clicks submit, POST the form data to the server.
+
+
+A single page app means that once the page loads, no user-generated actions on the page may cause the entire page to reload. 
+One way this rule could be broken in your implementation is through the use of an HTML form for submitting your JSON data. 
+You will know the page is reloading if you see a change in your URL. You must prevent this behavior from occurring.
+*/
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+var express = require('express');
 
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
 
+app.use(express.static('client'))
+ 
+app.listen(3000, () => console.log('app listening on port 3000'));
