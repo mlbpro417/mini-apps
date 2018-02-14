584c2cd8c16b03fdb33e4982f7a64bc09ad902b2
mlbpro417 <mlbpro417@gmail.com>
2018-02-13 16:28:17 -0800

    properly sending data to the server and starting helper functions to parse data for CSV

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -6,6 +6,96 @@ When the user clicks submit, POST the form data to the server.
 A single page app means that once the page loads, no user-generated actions on the page may cause the entire page to reload. 
 One way this rule could be broken in your implementation is through the use of an HTML form for submitting your JSON data. 
 You will know the page is reloading if you see a change in your URL. You must prevent this behavior from occurring.
+
+You may assume the JSON data has a regular structure and hierarchy (see included sample file). 
+The server must flatten the JSON hierarchy, mapping each item in the JSON to a single line of CSV report 
+(see included sample output). You may assume child records in the JSON will always be in a property called children 
+but you may not assume a JSON record has any other specific properties; i.e. any properties that exist besides children 
+must be mapped to a column in your CSV report.
 */
+
+
+// to do
+
+// make POST requests to the server
+  // link submit button in html to making a post request
+
+// user submitted data should be in json format 
+
+var dummyData = {
+    "firstName": "Joshie",
+    "lastName": "Wyattson",
+    "county": "San Mateo",
+    "city": "San Mateo",
+    "role": "Broker",
+    "sales": 1000000,
+    "children": [
+    {
+      "firstName": "Beth Jr.",
+      "lastName": "Johnson",
+      "county": "San Mateo",
+      "city": "Pacifica",
+      "role": "Manager",
+      "sales": 2900000,
+      "children": [
+        {
+          "firstName": "Smitty",
+          "lastName": "Won",
+          "county": "San Mateo",
+          "city": "Redwood City",
+          "role": "Sales Person",
+          "sales": 4800000,
+          "children": []
+        },
+        {
+          "firstName": "Allen",
+          "lastName": "Price",
+          "county": "San Mateo",
+          "city": "Burlingame",
+          "role": "Sales Person",
+          "sales": 2500000,
+          "children": []
+        }
+      ]
+    },
+    {
+      "firstName": "Beth",
+      "lastName": "Johnson",
+      "county": "San Francisco",
+      "city": "San Francisco",
+      "role": "Broker/Sales Person",
+      "sales": 7500000,
+      "children": []
+    }
+  ]
+};
+
+// handle click and submission of data
+var handleSubmitData = function() {
+  $('form').on('submit', function(e){
+  e.preventDefault();
+  var inputData = $('#input')[0].value;
+  sendData(dummyData);
+  // console.log();
+  });
+}();
+
+// function to send post request
+var sendData = function(message) {
+  $.ajax({
+    url: 'http://localhost:3000/',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('Message sent', data);
+    },
+    error: function (data) {
+      console.error('Failed to send message', data);
+    }
+  });
+}
+
+
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,64 @@
 var express = require('express');
 
 var app = express();
+
+app.use(express.static('client'));
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World');
 });
 
-app.use(express.static('client'))
- 
+app.post('/', function (req, res) {
+  var body = [];
+  req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      res.end(body);
+    });
+});
+
+
+// each property on the object needs to be sent as the first line in the csv data
+// each additional line is the data from the top to the bottom in the structure
+// so first line is the very first person in the object and each additional is the person under him
+// recurse through the object to check to see if the person has children or not
+
+// break into two functions, first one simply puts out the first line 
+
+var categoryGrabber = function(teamData) {
+  var results = [];
+  for (var key in teamData) {
+    if (key !== 'children') {
+      results.push(key);
+    }
+  }
+  return results.toString();
+};
+
+var personalDataGrabber = function (teamData) {
+  var results = [];
+  for (var key in teamData) {
+    var value = teamData[key];
+    if (key !== 'children') {
+      results.push(value);
+    }
+  }
+  return results.toString();
+
+  var recurseData = function
+
+   if (teamData.children.length === 0) {
+    return teamData.individualSales;
+  } else {
+    // iterate through one level below 
+    var sales = 0;
+    for (var i = 0; i < teamData.children.length; i++) {
+      sales += totalSales(teamData.children[i]);
+    }
+    return sales + teamData.individualSales;
+    
+  }
+};
+
 app.listen(3000, () => console.log('app listening on port 3000'));
