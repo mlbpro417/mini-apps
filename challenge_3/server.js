bda3660af087c4e84516a4847a46ade008e7d59f
mlbpro417 <mlbpro417@gmail.com>
2018-02-14 11:27:01 -0800

    board in html and simple CSS

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -5,4 +5,4 @@ app.use(express.static('client'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!')); 
+app.listen(3000, () => console.log('app listening on port 3000')); 
