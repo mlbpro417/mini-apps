b31d0960401283fd25ec2e4015c2df0eccc90861
mlbpro417 <mlbpro417@gmail.com>
2018-02-14 15:19:11 -0800

    added Board component and styles.css

--- challenge_3/client/index.jsx
+++ challenge_3/client/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
+import Board from './Board.jsx'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render () {
     return (
       <div>
-        <p> Hello React!</p>;
-        <AwesomeComponent />
+        <p> Hello React!</p>
+        
+        <Board
+        id="board" />
       </div>
     )
   }
