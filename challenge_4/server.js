61f9e14d1bbf0d69d10317c40570ce2c90fc8926
mlbpro417 <mlbpro417@gmail.com>
2018-02-15 11:12:41 -0800

    webpack and dependencies installed

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('client'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('app listening on port 3000')); 
 
